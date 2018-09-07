fe50cfb818d094b38c5e4e5807299398a09b684d
flaeteh <flaeteh@gmail.com>
2018-09-07 23:20:20 +0800

    Linear, Relative, Constraint

---
+++ app/src/main/java/florence/teh/com/tehflorencelab1/ConstraintLayoutActivity.kt
@@ -0,0 +1,12 @@
+package florence.teh.com.tehflorencelab1
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ConstraintLayoutActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_constraint_layout)
+    }
+}
---
+++ app/src/main/java/florence/teh/com/tehflorencelab1/LinearLayoutActivity.kt
@@ -0,0 +1,12 @@
+package florence.teh.com.tehflorencelab1
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class LinearLayoutActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_linear_layout)
+    }
+}
